f98a0d21f1078dbf9387ebc787faa961f9924846
karina <karina.renteria.valencia@utelvt.edu.ec>
2022-09-19 09:45:29 -0500

    Suma.cpp

---
+++ suma.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	float x,y,z;
+cout<<"Ingrese 2 numeros separados de espacio x y :";
+	cin>>x>>y;
+	z=x+y;
+	cout<<z;
+	cout<<"El resultado es :";
+	cout<<z<<endl;
+	return 0;
+}
