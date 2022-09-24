5a5f82edd3d5ce889199c3b48c521606b958e598
karina <karina.renteria.valencia@utelvt.edu.ec>
2022-09-24 10:32:47 -0500

    velocimetro

--- velocimetro.cpp
+++ velocimetro.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int  main()
 {
-	float KIRV_v,KIRV_d,KIRV_t;
-	cout<< " Ingrese la distancia (en km) KIRV_d= " ;
-	cin>>KIRV_d;
-	cout<< " Ingrese el tiempo (en horas) KIRV_t= " ;
-	cin>>KIRV_t;
-	KIRV_v=KIRV_d/KIRV_t;
-	cout<< " La velocidad calculada es de KIRV_v= " <<KIRV_t<<endl<<endl;
+	float v,d,t;
+	cout<< " Ingrese la distancia (en km) d= " ;
+	cin>>d;
+	cout<< " Ingrese el tiempo (en horas) t= " ;
+	cin>>t;
+	v=d/t;
+	cout<< " La velocidad calculada es de v= " <<t<<endl<<endl;
 	return  0 ;
 
 }
