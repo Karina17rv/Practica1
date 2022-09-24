4d2822e02385db32957c13d46a62b18f241aca89
karina <karina.renteria.valencia@utelvt.edu.ec>
2022-09-24 10:44:06 -0500

    contabiliza

--- contabiliza.cpp
+++ contabiliza.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main()
 {
-	int KIRV_i=0,KIRV_l;
-	float KIRV_x,KIRV_s=0;
-	cout<<"ingrese el limite KIRV_l=";
-	cin>>KIRV_l;
+	int i=0,l;
+	float x,s=0;
+	cout<<"ingrese el limite l=";
+	cin>>l;
 	{	
-	cout<<"ingrese el numero KIRV_x="; 
-	cin>>KIRV_x;
-	KIRV_i=KIRV_i+1;
-	KIRV_s=KIRV_s+KIRV_x;
+	cout<<"ingrese el numero x="; 
+	cin>>x;
+	i=i+1;
+	s=s+x;
 	}
-        while(KIRV_i<KIRV_l);
-	cout<<"Se ingresaron "<<KIRV_l<<" numeros "<< "que sumaron "<<KIRV_s<<endl;
+        while(i<l);
+	cout<<"Se ingresaron "<<l<<" numeros "<< "que sumaron "<<s<<endl;
 	return 0;
 }
